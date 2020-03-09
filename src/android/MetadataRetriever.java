d786e4c2dcc8a2602ea0810cfd619c1ef493056e
Jan Wojcicki <janwojcicki45@gmail.com>
2020-03-09 17:32:27 +0100

    more options to be retrieved

--- src/android/MetadataRetriever.java
+++ src/android/MetadataRetriever.java
@@ -16,27 +16,89 @@ import java.io.FileOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.cordova.PluginResult;
 import org.apache.cordova.PluginResult.Status;
+import org.json.JSONObject;
+import java.util.Map;
 
 public class MetadataRetriever extends CordovaPlugin {
+	private static final String ERR_INVALID_OPTIONS = "ERR_INVALID_OPTIONS";
+	private static final String ERR_NOT_INITIALIZED = "ERR_NOT_INITIALIZED";
 	private final String TAG = MetadataRetriever.class.getSimpleName();
+	private MediaMetadataRetriever mediaMetadataRetriever = new MediaMetadataRetriever();
+	private Map<String, Integer> possibleQueries;
 
 	@Override
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+		possibleQueries.put("Album", MediaMetadataRetriever.METADATA_KEY_ALBUM);
+		possibleQueries.put("Artist", MediaMetadataRetriever.METADATA_KEY_ARTIST);
+		possibleQueries.put("Duration", MediaMetadataRetriever.METADATA_KEY_DURATION);
+		possibleQueries.put("Title", MediaMetadataRetriever.METADATA_KEY_TITLE);
 		Log.d(TAG, "metadata retriever initialized");
 	}
 
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		if (action.equals("getMetadata")) {
-			MediaMetadataRetriever mediaMetadataRetriever = new MediaMetadataRetriever();
-			mediaMetadataRetriever.setDataSource(args.getJSONObject(0).getString("path"));
+		JSONObject params = args.getJSONObject(0);
 
-			String duration = mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
-			final PluginResult result = new PluginResult(PluginResult.Status.OK, duration);
-			callbackContext.sendPluginResult(result);
+		if (params == null) {
+			callbackContext.error(ERR_INVALID_OPTIONS);
+			return false;
 		}
+
+		JSONArray filePaths = new JSONArray();
+
+		if (params.isNull("path")) {
+			if (params.isNull("paths")) {
+				callbackContext.error(ERR_INVALID_OPTIONS);
+				return false;
+			}
+
+			filePaths = params.getJSONArray("paths");
+
+		} else {
+			filePaths.put(params.getString("path"));
+		}
+
+		JSONArray returnObj = new JSONArray();
+
+		for (int i = 0; i < filePaths.length(); i++) {
+			mediaMetadataRetriever.setDataSource(filePaths.getString(i));
+
+			JSONObject thisFileObject = new JSONObject();
+
+			for (Map.Entry<String, Integer> entry : possibleQueries.entrySet()) {
+				if (action.equals("get" + entry.getKey())) {
+					addMetadataToJSON(entry.getKey(), entry.getValue(), thisFileObject);
+				}
+			}
+
+			if (action.equals("getMetadata")) {
+				for (Map.Entry<String, Integer> entry : possibleQueries.entrySet()) {
+					addMetadataToJSON(entry.getKey(), entry.getValue(), thisFileObject);
+				}
+			}
+
+			returnObj.put(thisFileObject);
+		}
+
+		final PluginResult result;
+		if (returnObj.length() == 1) {
+			result = new PluginResult(PluginResult.Status.OK, returnObj.getJSONObject(0));
+		} else {
+			result = new PluginResult(PluginResult.Status.OK, returnObj);
+		}
+
+		callbackContext.sendPluginResult(result);
+		return true;
+	}
+
+	private JSONObject addMetadataToJSON(String key, int metadataID, JSONObject obj) throws JSONException {
+		String meta = mediaMetadataRetriever.extractMetadata(metadataID);
+		obj.put(key, meta);
+		return obj;
 	}
 }
--- www/MetadataRetriever.js
+++ www/MetadataRetriever.js
@@ -1,6 +1,26 @@
 var exec = require('cordova/exec');
 var pluginName = 'MetadataRetriever';
 
-exports.getMetadata = function(success, error, filePath) {
-	exec(success, error, pluginName, 'getMetadata', [filePath]);
+function prepareArgs(args) {
+	if (Array.isArray(args)) {
+		return { paths: args };
+	} else {
+		return { paths: [args] };
+	}
+}
+
+exports.getMetadata = function(success, error, args) {
+	exec(success, error, pluginName, 'getMetadata', [prepareArgs(args)]);
+};
+exports.getAlbum = function(success, error, args) {
+	exec(success, error, pluginName, 'getAlbum', [prepareArgs(args)]);
+};
+exports.getArtist = function(success, error, args) {
+	exec(success, error, pluginName, 'getArtist', [prepareArgs(args)]);
+};
+exports.getDuration = function(success, error, args) {
+	exec(success, error, pluginName, 'getDuration', [prepareArgs(args)]);
+};
+exports.getTitle = function(success, error, args) {
+	exec(success, error, pluginName, 'getTitle', [prepareArgs(args)]);
 };
